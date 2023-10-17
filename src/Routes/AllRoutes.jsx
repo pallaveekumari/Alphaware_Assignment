a4733be361cbfc1a794c7f3d27a31ee707ab074e
pallaveekumari <pallavee.kumari@razorpay.com>
2023-10-17 22:22:35 +0530

    basic_setup_completed

---
+++ src/Routes/AllRoutes.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Route,Routes} from "react-router-dom"
+import HomePage from './HomePage/HomePage'
+import Cart from './Cart/Cart'
+import Products from './Products/Products'
+const AllRoutes = () => {
+  return (
+  <Routes>
+    <Route path="/" element={<HomePage/>}/>
+
+    <Route path="/cart" element={<Cart/>}/>
+
+    <Route path="/product" element={<Products/>}/>
+  </Routes>
+  )
+}
+
+export default AllRoutes
