47837a105ba1b32b71eb259ae9b0e2e9f2fbf8eb
pallaveekumari <pallavee.kumari@razorpay.com>
2023-10-17 23:04:56 +0530

    created-navbar-homepage

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom';
+import styles from "./Navbar.module.css"
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
-    <div>Navbar</div>
-  )
+    <div className={styles.navbar}>
+      <div onClick={() => navigate("/")} className={styles.navIcon}>
+        Home
+      </div>
+      <div onClick={() => navigate("/products")} className={styles.navIcon}>
+        Products
+      </div>
+      <div onClick={() => navigate("/cart")} className={styles.navIcon}>
+        Cart
+      </div>
+    </div>
+  );
 }
 
 export default Navbar
--- src/Routes/HomePage/HomePage.jsx
+++ src/Routes/HomePage/HomePage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import styles from "./HomePage.module.css"
+import Navbar from '../../Components/Navbar/Navbar'
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className={styles.homepageImg}>
+       <Navbar />
+      <p className={styles.heading}>Alphaware</p>
+    </div>
   )
 }
 
